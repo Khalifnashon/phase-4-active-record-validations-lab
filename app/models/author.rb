ad58ca5f17c72e425ba967f44add94d8aa24b0c4
+ <+>
2023-03-15 21:44:23 +0300

    completed lab

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, format: { with: /\A[A-Z]/, message: "must start with a capital letter" }
+    validates :phone_number, presence: true, length: { is: 10 }
 end
+  
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, uniqueness: { scope: :category }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  
+    validate :title_uniqueness_within_category
+    validate :sufficiently_clickbait_y_title
+
+    private
+  
+    def title_uniqueness_within_category
+      if Post.where(category: category, title: title).exists?
+        errors.add(:title, "must be unique within the same category")
+      end
+    end
+
+    def sufficiently_clickbait_y_title
+        if title.present? && !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+          errors.add(:title, "must be sufficiently clickbait-y")
+        end
+    end
 end
+  
+  
+  
+  
+  
+  
